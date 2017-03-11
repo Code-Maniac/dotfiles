83502e9884f47198d5c6018352ee45127e594065
Nick Jaycock <jaycock.n@gmail.com>
2017-03-11 00:34:51 +0000

    removed unneccesary files

--- install_fonts.sh
+++
@@ -1,6 +0,0 @@
-rm -rf ./fonts
-git clone https://github.com/powerline/fonts
-cd fonts
-./install.sh
-cd ..
-rm -rf ./fonts
--- install_vimplug.sh
+++
@@ -1,2 +0,0 @@
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-	http://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
