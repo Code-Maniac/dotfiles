d283be40d01ac6b3fcc4172a6d7c16f57e4a9221
Nick Jaycock <jaycock.n@gmail.com>
2023-04-13 11:06:39 +0100

    Add ideavimrc

--- setup-windows.ps1
+++ setup-windows.ps1
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 echo "Install chocolatey"
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
@@ -38,5 +39,8 @@ cp ./winterm/settings.json $env:LOCALAPPDATA/Packages/Microsoft.WindowsTerminal_
 
 echo "Copy powershell profile"
 cp ./powershell/Microsoft.PowerShell_profile.ps1 $PROFILE
+
+echo "Copy .ideavimrc"
+cp ./vim/ideavimrc $env:USERPROFILE/.ideavimrc
 
 echo "All finished"
