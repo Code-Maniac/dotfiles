b6fb931bce89372f737c3fbca9745c788985b2ae
Nick Jaycock <nickj@evinox.co.uk>
2024-01-14 10:46:11 +0000

    Fix setup windows + add background

--- setup-windows.ps1
+++ setup-windows.ps1
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 echo "Install chocolatey"
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
